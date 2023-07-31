09c0a826d5df8bca3f77559efa16a7f7d79e5d81
jashan <jashannoor2001@gmail.com>
2023-07-31 11:51:46 -0600

    Redesign Footer

--- src/views/components/footer.tsx
+++ src/views/components/footer.tsx
@@ -1,13 +1,25 @@
 import React from "react";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
-function footer() {
+function Footer() {
   return (
     <footer>
       <div>
-        <h5>&copy; 2023 jshnsaini</h5>
+        <div>
+          <a target="_blank" className="icon" href="https://www.linkedin.com/in/jshnsaini/"><LinkedInIcon /></a>
+          <a target="_blank" className="icon" href="https://github.com/jshn16"><GitHubIcon /></a>
+          <a target="_blank" className="icon" href="https://www.instagram.com/jshnsaini/"><InstagramIcon /></a>
+        </div>
+        <div>
+          <span>&copy; 2023 jshnsaini</span>
+        </div>
+
+
       </div>
     </footer>
   );
 }
 
-export default footer;
+export default Footer;
